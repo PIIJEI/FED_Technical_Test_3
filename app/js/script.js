aeb4a362ac78c207751e301e955a4338556a3a0e
Piotr Osiak <piotr.osiak@hotmail.co.uk>
2017-08-14 17:25:05 +0100

    Completed Front End Test

--- app/js/script.js
+++ app/js/script.js
@@ -6,13 +6,13 @@ var body = $('body'),
 
 /* Callback Functions */
 function dialog() {
-	$('#dialogPopUp').css("display","block");
+	$('#dialogPopUp').fadeIn(400).css("display","block");
 	$('body').css("overflow","hidden");
 	return;
 }
 
 function dialogClose() {
-	$('#dialogPopUp').css("display","none");
+	$('#dialogPopUp').fadeOut(400).css("display","none");
 	$('body').css("overflow","visible");
 	$(document).keyup(function(e){
 		if(e.which == '27') {
@@ -22,3 +22,29 @@ function dialogClose() {
 	});
 	return;
 }
+
+function popUpTip() {
+	$('.tooltip').fadeIn().css("display","block");
+	return;
+}
+
+function closeTip() {
+	$('.tooltip').css("display","none");
+	return;
+}
+
+$("#sales").on("click",function(){
+	$("#sales").addClass("active-report");
+	$("#tickets").removeClass("active-report");
+	$("#graph__sales").removeClass("displayHidden");
+	$("#graph__tickets").addClass("displayHidden");
+});
+
+$("#tickets").on("click",function(){
+	$("#tickets").addClass("active-report");
+	$("#sales").removeClass("active-report");
+	$("#graph__tickets").removeClass("displayHidden");
+	$("#graph__sales").addClass("displayHidden");
+});
+
+
